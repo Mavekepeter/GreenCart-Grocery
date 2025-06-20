29e9eb27d464387377e31bd892b4df6cd33c425d
samuel maveke <mavekepeter2022@gmail.com>
2025-06-20 13:16:06 +0300

    Addeded seller access control

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -30,3 +30,72 @@ export const registerUser = async(req,res)=>{
         return res.json({success:false, message:error.message})
     }
 }
+
+//login User : /api/user/login
+export const login = async (req,res)=>{
+    try {
+        const {email, password} = req.body
+        if (!email || !password) {
+            return res.json({success:false, message:"Email and passsword are required"})
+        }
+        const user = await User.findOne({email});
+        if (!user) {
+            return res.json({success:false, message:"Invalid email or password"})
+        }
+        const isMatch = await bcryptjs.compare(password,user.password)
+        if (!isMatch) {
+          return res.json({success:false, message:"Invalid email or password"})
+
+        }
+
+        const token = jwt.sign({id: user._id},process.env.JWT_SECTRET,{expiresIn:'7d'})
+        res.cookie('token',token,{
+            httpOnly:true, //prevent javascript to access cookies
+            secure:process.env.NODE_ENV === 'production',//use secure cooies in production
+
+            sameSite:process.env.NODE_ENV === 'production' ? 'none' : 'strict', //CSRF protection
+            maxAge:7*24*60*1000,
+        })
+        return res.json({success:true, user:{email:user.email,name:user.name}})
+    } catch (error) {
+        console.log(error.message);
+        return res.json({success:false, message:error.message})
+    }
+}
+
+//check Auth : /api/user/is-auth
+export const isAuth = async(req,res)=>{
+    try {
+        const {userId} = req.body;
+        const user = await User.findById(userId).select("-password")
+        return res.json({success:true, user})
+
+
+    } catch (error) {
+        console.log(error.message);
+        return res.json({success:false, message:error.message})
+
+    }
+}
+//Logout User :/api/user/logout
+export const logout =async(req,res)=>{
+    try {
+        res.clearCookie('token',{
+            httpOnly:true, //prevent javascript to access cookies
+            secure:process.env.NODE_ENV === 'production',//use secure cooies in production
+            sameSite:process.env.NODE_ENV === 'production' ? 'none' : 'strict', //CSRF protection
+            
+        });
+        return res.json({success:true,message:"Logged out"})
+
+    } catch (error) {
+        console.log(error.message);
+        return res.json({success:false, message:error.message})
+    }
+
+}
+
+
+
+
+
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,7 +1,12 @@
 import express from 'express'
-import { registerUser } from '../controllers/userController.js';
+import { isAuth, login, logout, registerUser } from '../controllers/userController.js';
+import authUser from '../middlewares/authUser.js';
 const userRouter = express.Router();
 
-userRouter.post('/register',registerUser)
+userRouter.post('/register',registerUser);
+userRouter.post('/login',login);
+userRouter.get('/is-auth',authUser,isAuth);
+userRouter.get('/logout',authUser,logout);
+
 
 export default userRouter
